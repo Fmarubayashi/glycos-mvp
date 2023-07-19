72522270040e9785bf6aea00bfb64854be52513f
Fernando Marubayashi <FERNANDO.MARUBAYASHI1@GMAIL.COM>
2023-07-19 15:33:02 -0300

    testing another fallback for observations

--- src/pages/observations/index.tsx
+++ src/pages/observations/index.tsx
@@ -23,21 +23,21 @@ const Observations: React.FC = () => {
     <main className="flex flex-col min-h-screen px-4 py-2 md:px-12 md:py-8 items-center">
       <h1 className="text-2xl my-4">Observações médicas</h1>
 
-      {observations[0]?.observation ? (
+      {observations.length > 0 ? (
         <Card className="w-full max-w-[900px] flex flex-col h-fit">
           <p className="sm:text-lg text-sm flex-grow h-full">
-            {observations[0]?.observation}
+            {observations[0].observation}
           </p>
           <p className="text-sm text-gray-500 mt-2">
             -
-            {observations[0]?.doctor.firstName +
+            {observations[0].doctor.firstName +
               " " +
-              observations[0]?.doctor.lastName}
+              observations[0].doctor.lastName}
           </p>
           <p className="text-sm text-gray-500 mt-2">
-            {observations[0]?.created_at &&
+            {observations[0].created_at &&
               format(
-                new Date(observations[0]?.created_at?.replace(" ", "T")),
+                new Date(observations[0].created_at.replace(" ", "T")),
                 "dd/MM/yy | HH:mm"
               )}
           </p>
