8aa67531b1937c506a9fadb8674419c9e7f1d5ae
Fernando Marubayashi <FERNANDO.MARUBAYASHI1@GMAIL.COM>
2023-07-19 09:39:46 -0300

    pwa working

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="../../public/icon-512x512.png"></link>
+        <meta name="theme-color" content="#f69435" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
