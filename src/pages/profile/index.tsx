40908a2cdcd1da7a2222cd96e4f540b498beddeb
Fernando Marubayashi <FERNANDO.MARUBAYASHI1@GMAIL.COM>
2023-07-19 16:10:02 -0300

    adding default profile picture

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,10 +2,11 @@ import api from "@/services/api";
 import { Measure } from "@/types/Measure";
 import { User, UserRole, UserRoleLabel } from "@/types/User";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import defaultIcon from "../../../public/default-user-icon.jpg";
 
 const Profile: React.FC = () => {
   const [user, setUser] = useState<User>();
-
   useEffect(() => {
     async function getUser() {
       try {
@@ -22,6 +23,13 @@ const Profile: React.FC = () => {
     <main className="flex min-h-screen flex-col items-center pt-12">
       <h2 className="text-2xl">Dados do usuário</h2>
       <div className="flex flex-col items-center pt-12 w-full justify-between">
+        <div className="flex items-center justify-center rounded-full bg-gray-200 w-1/4 h-1/4">
+          <Image
+            src={defaultIcon}
+            alt={"User Image"}
+            className="rounded-full"
+          />
+        </div>
         <span>Nome: {fullName()}</span>
         <span>Email: {user?.email}</span>
         <span>CPF: {cpfMask()}</span>
